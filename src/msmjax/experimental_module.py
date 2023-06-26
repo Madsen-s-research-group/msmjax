fc99bc1f64555d39fbe32d220c2082aa88177c2a
Florian <florian.buchner@tuwien.ac.at>
2023-06-26 10:38:08 +0200

    Add slides.

--- src/msmjax/experimental_module.py
+++ src/msmjax/experimental_module.py
@@ -1,6 +1,7 @@
+import jax
 import jax.numpy as jnp
 import numpy as onp
-from jax_md.partition import neighbor_list
+from jax_md import energy, partition, quantity, space
 
 if __name__ == "__main__":
     print("What's up")
