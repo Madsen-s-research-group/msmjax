18baf24a8eba77faa94600d03484d442fb2fba8b
Florian <florian.buchner@tuwien.ac.at>
2023-06-27 14:02:18 +0200

    WIP: splitting of 1/r kernel.

--- src/msmjax/kernel_splitting.py
+++ src/msmjax/kernel_splitting.py
@@ -11,6 +11,8 @@
         of Illinois at Urbana-Champaign, 2006.
 """
 
+from typing import Callable
+
 import jax
 import jax.numpy as jnp
 import numpy as onp
@@ -40,3 +42,64 @@ class SofteningFunctionOneOverR:
             rho < 1.0, jnp.polyval(self.coeffs, rho * rho - 1.0), 1.0 / rho
         )
         return nruter
+
+
+def build_splitting_one_over_r(
+    max_level: int, level_zero_cutoff: float, softening_function: Callable
+):
+    """Split kernel 1/r in (max_order + 1) terms g_l(r) according to reference.
+
+    The splitting terms sum up to the Coulomb kernel 1/r like this:
+    1/r = g_0(r) + g_1(r) + g_2(r) + ... + g_{max_order}(r)
+
+    Args:
+        max_level: The number of splits to be performed.
+        level_zero_cutoff: The cutoff radius of the level-zero kernel function
+            (= of the direct term in the kernel splitting).
+        softening_function: The basic smoothing function for this splitting.
+
+    Returns:
+        A list of one-argument functions g_l with l from zero to `max_order`
+        that represent the summands in the splitting of the interaction
+        kernel. These have their respective cutoff 'built in' already and
+        their arguments must be given in the same length units that
+        `level_zero_cutoff` was supplied in .
+
+    Raises:
+        ValueError: If the arguments do not make sense.
+    """
+    if not isinstance(max_level, (int, onp.integer, jnp.integer)):
+        raise ValueError('"max_order" must be an integer')
+    if max_level < 1:
+        raise ValueError("the expansion must have at least one term")
+    if level_zero_cutoff <= 0.0:
+        raise ValueError('"cutoff" must be a positive number')
+    # TODO
+    # if not isinstance(softening_function, SmoothingFunction):
+    #     raise ValueError("\"smoothing\" must be a SmoothingFunction instance")
+
+    def gamma_0(rho):
+        return 1.0 / rho - softening_function(rho)
+
+    def gamma_l(rho):
+        return 2.0 * softening_function(2.0 * rho) - softening_function(rho)
+
+    def gamma_L(rho):
+        return 2.0 * softening_function(2.0 * rho)
+
+    def g_l_factory(a_l, gamma):
+        def g_l(r):
+            nonlocal a_l, gamma
+            return gamma(r / a_l) / a_l
+
+        return g_l
+
+    all_gammas = [gamma_0] + [gamma_l] * (max_level - 1) + [gamma_L]
+
+    nruter = []
+    a_l = level_zero_cutoff
+    for ell, gamma in enumerate(all_gammas):
+        nruter.append(g_l_factory(a_l, gamma))
+        a_l *= 2.0
+
+    return nruter
