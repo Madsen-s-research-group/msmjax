def41338b23ab64606cb9b8e153cccbadaf6c06e
Florian <florian.buchner@tuwien.ac.at>
2023-10-13 15:29:49 +0200

    Clean up

--- src/msmjax/bspline_basis.py
+++ src/msmjax/bspline_basis.py
@@ -7,26 +7,7 @@ import jax
 import jax.numpy as jnp
 from jax.typing import ArrayLike
 
-
-def _divide_zero_safe(
-    numerator: jnp.ndarray,
-    denominator: jnp.ndarray,
-) -> jnp.ndarray:
-    """Function that forces the result of dividing by 0 to be equal to 0.0
-    in a jit- and autodiff-compatible way
-
-    Args:
-        numerator: Values in the numerator
-        denominator: Values in the denominator, may contain zeros
-    Returns:
-        numerator / denominator with result == 0.0 where denominator == 0.0
-    """
-    denominator_masked = jnp.where(denominator == 0.0, 1.0, denominator)
-    return jnp.where(
-        denominator == 0.0,
-        0.0,
-        numerator / denominator_masked,
-    )
+from msmjax.utils import _divide_zero_safe
 
 
 def characteristic(knots: jnp.ndarray, eval_point: ArrayLike) -> jnp.ndarray:
--- src/msmjax/experimental_module.py
+++
@@ -1,9 +0,0 @@
-import jax
-import jax.numpy as jnp
-import numpy as onp
-from jax_md import energy, partition, quantity, space
-
-if __name__ == "__main__":
-    print("What's up")
-
-    pass
