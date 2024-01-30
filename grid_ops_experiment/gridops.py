0d63056d18b20d18a11548ec7bffebc7185d9551
Florian <florian.buchner@tuwien.ac.at>
2024-01-30 17:25:03 +0100

    clean up and refactor

--- grid_ops_experiment/gridops.py
+++ grid_ops_experiment/gridops.py
@@ -81,9 +81,10 @@ def create_anterpolation_function(grid: GridAxis1D):
     return anterpolate
 
 
-def make_restriction_function(
-    grid: GridAxis1D, grid_below: GridAxis1D, J: npt.ArrayLike
+def make_restriction_operator(
+    grid_source: GridAxis1D, grid_target: GridAxis1D, J: npt.ArrayLike
 ) -> Callable:
+    # TODO: full J or J_zeroplus part only?
     J = jnp.asarray(J)
 
     # TODO: How should p be passed/inferred? Probably should be an attribute of the grid?
@@ -92,43 +93,46 @@ def make_restriction_function(
     def get_gridinds_one_below(m_raw: int) -> jax.Array:
         # TODO: would 2 * m_raw + jnp.arange(-p // 2, p // 2 + 1) be better readable (assuming that is really correct and the same)?
         n_raw_selected = (2 * m_raw - p // 2) + jnp.arange(p + 1)
-        n_selected = grid_below.process_raw_indices(n_raw_selected)
+        n_selected = grid_source.process_raw_indices(n_raw_selected)
         return n_selected
 
-    if grid.periodic:
+    if grid_target.periodic:
         raise  # TODO: not implemented
     else:
         # TODO: should this be handled by a generic grid function as well? (how?)
-        raw_ms = jnp.arange(grid.n_total) - p // 2
-        ms = grid.process_raw_indices(raw_ms)
+        raw_ms = jnp.arange(grid_target.n_total) - p // 2
+        ms = grid_target.process_raw_indices(raw_ms)
 
-    def apply_restrict(gridcharge_below: jax.Array) -> jax.Array:
+    def restrict(array_fine: jax.Array) -> jax.Array:
         selected_ns = jax.vmap(get_gridinds_one_below)(raw_ms)
         is_in_bounds = jnp.logical_and(
-            selected_ns >= 0, selected_ns < grid_below.n_total
+            selected_ns >= 0, selected_ns < grid_source.n_total
         )
-        intentionally_out_of_bounds_index = grid_below.n_total + 1
+        intentionally_out_of_bounds_index = grid_source.n_total + 1
         selected_ns = jnp.where(
             is_in_bounds, selected_ns, intentionally_out_of_bounds_index
         )
-        selected_gridcharges_below = gridcharge_below.at[selected_ns].get(
+        selected_gridcharges_below = array_fine.at[selected_ns].get(
             mode="fill", fill_value=0.0
         )
 
-        gridcharge = jnp.zeros(grid.n_total)
-        gridcharge = gridcharge.at[ms].add(
+        array_coarse = jnp.zeros(grid_target.n_total)
+        array_coarse = array_coarse.at[ms].add(
             (selected_gridcharges_below * J).sum(axis=1)
         )
 
-        return gridcharge
+        return array_coarse
 
-    return apply_restrict
+    return restrict
 
 
 def make_prolongation_operator(
-    grid: GridAxis1D, grid_above: GridAxis1D, p: int, J_zeroplus: npt.ArrayLike
+    grid_source: GridAxis1D,
+    grid_target: GridAxis1D,
+    p: int,
+    J_zeroplus: npt.ArrayLike,
 ):
-    # TODO: J
+    # TODO: J (pass full or only zeroplus part?)
     J_zeroplus = jnp.asarray(J_zeroplus)
 
     start_even = onp.ceil(onp.round(-p / 4, decimals=1)).astype(int)
@@ -136,53 +140,56 @@ def make_prolongation_operator(
     start_odd = onp.ceil(onp.round(0.5 - p / 4, decimals=1)).astype(int)
     end_odd = onp.floor(onp.round(0.5 + p / 4, decimals=1)).astype(int)
 
-    displacements_even = jnp.arange(start_even, end_even + 1)
-    displacements_odd = jnp.arange(start_odd, end_odd + 1)
+    dists_to_neighboring_ms_even = jnp.arange(start_even, end_even + 1)
+    dist_to_neighboring_ms_odd = jnp.arange(start_odd, end_odd + 1)
 
-    inds_into_J_even = -2 * displacements_even
-    inds_into_J_odd = 1 - 2 * displacements_odd
+    inds_into_J_even = -2 * dists_to_neighboring_ms_even
+    inds_into_J_odd = 1 - 2 * dist_to_neighboring_ms_odd
 
     def get_ns_one_above_even(m_raw: int):
-        n_raw_selected = m_raw // 2 + displacements_even
-        return grid_above.process_raw_indices(n_raw_selected)
+        n_raw_selected = m_raw // 2 + dists_to_neighboring_ms_even
+        return grid_source.process_raw_indices(n_raw_selected)
 
     def get_ns_one_above_odd(m_raw: int):
-        n_raw_selected = m_raw // 2 + displacements_odd
-        return grid_above.process_raw_indices(n_raw_selected)
+        n_raw_selected = m_raw // 2 + dist_to_neighboring_ms_odd
+        return grid_source.process_raw_indices(n_raw_selected)
 
-    if grid.periodic:
+    if grid_target.periodic:
         raise  # TODO: not implemented
+        # slice_even = slice(0, None, 2)
+        # slice_odd = slice(1, None, 2)
     else:
         # TODO: should this be handled by a generic grid function as well? (how?)
-        raw_ms = jnp.arange(grid.n_total) - p // 2
-        ms = grid.process_raw_indices(raw_ms)
-        if (p // 2) % 2 == 0:
-            slice_even = slice(0, None, 2)
-            slice_odd = slice(1, None, 2)
-        else:
-            slice_even = slice(1, None, 2)
-            slice_odd = slice(0, None, 2)
+        raw_ms = jnp.arange(grid_target.n_total) - p // 2
+        ms = grid_target.process_raw_indices(raw_ms)
+        slice_even = slice((p // 2) % 2, None, 2)
+        slice_odd = slice(1 - (p // 2) % 2, None, 2)
+        # TODO
+        # if (p // 2) % 2 == 0:
+        #     slice_even = slice(0, None, 2)
+        #     slice_odd = slice(1, None, 2)
+        # else:
+        #     slice_even = slice(1, None, 2)
+        #     slice_odd = slice(0, None, 2)
 
-    def prolongate(gridarray_above: jax.Array) -> jax.Array:
-        gridarray = jnp.zeros(grid.n_total)
-
+    def prolongate(array_coarse: jax.Array) -> jax.Array:
         # TODO: the identification of odd/even is wrong when p // 2 is odd!
         ns_even_ms = jax.vmap(get_ns_one_above_even)(raw_ms[slice_even])
         ns_odd_ms = jax.vmap(get_ns_one_above_odd)(raw_ms[slice_odd])
 
-        gridarray = gridarray.at[ms[slice_even]].add(
+        array_fine = jnp.zeros(grid_target.n_total)
+        array_fine = array_fine.at[ms[slice_even]].add(
             (
-                gridarray_above[ns_even_ms]
+                array_coarse[ns_even_ms]
                 * J_zeroplus[jnp.abs(inds_into_J_even)]
             ).sum(axis=1)
         )
-        gridarray = gridarray.at[ms[slice_odd]].add(
+        array_fine = array_fine.at[ms[slice_odd]].add(
             (
-                gridarray_above[ns_odd_ms]
-                * J_zeroplus[jnp.abs(inds_into_J_odd)]
+                array_coarse[ns_odd_ms] * J_zeroplus[jnp.abs(inds_into_J_odd)]
             ).sum(axis=1)
         )
 
-        return gridarray
+        return array_fine
 
     return prolongate
