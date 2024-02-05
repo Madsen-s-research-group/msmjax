03f8e0a3b81db486c6d6d22eff8683886657a6a5
Florian <florian.buchner@tuwien.ac.at>
2024-02-05 15:47:26 +0100

    WIP: compute grid force contrib using derivatives of splines only (rather than derivatives of full energy)

--- grid_ops_experiment/gridops.py
+++ grid_ops_experiment/gridops.py
@@ -1,3 +1,4 @@
+import functools
 from typing import Callable, NamedTuple, Tuple
 
 import jax
@@ -42,13 +43,24 @@ def set_up_grid_axis(length: float, h: float, p: int, periodic: bool):
 
     bspline_basis_element = create_bspline_basis_element(order=p - 1)
 
-    def get_gridindices_and_splinevals(x: float):
+    def get_gridindices_and_splinevals(
+        x: float, return_spline_gradients: bool = False
+    ):
         x_over_h = x / h
         reference_index = jnp.ceil(x_over_h).astype(int)
         raw_indices = reference_index + jnp.arange(-p // 2, p // 2)
-        splinevals = jax.vmap(bspline_basis_element)(x_over_h - raw_indices)
         # TODO: should this return raw or processed indices?
-        return process_raw_indices(raw_indices), splinevals
+        indices = process_raw_indices(raw_indices)
+        if return_spline_gradients:
+            spline_outputs = jax.vmap(
+                jax.value_and_grad(bspline_basis_element)
+            )(x_over_h - raw_indices)
+        else:
+            spline_outputs = (
+                jax.vmap(bspline_basis_element)(x_over_h - raw_indices),
+            )
+
+        return indices, spline_outputs
 
     return GridAxis1D(
         periodic=periodic,
@@ -61,22 +73,29 @@ def set_up_grid_axis(length: float, h: float, p: int, periodic: bool):
     )
 
 
-def create_anterpolation_function(grid: GridAxis1D):
+def create_anterpolation_function(
+    grid: GridAxis1D, return_spline_gradients: bool = False
+):
+    get_gridindices_and_splinevals = functools.partial(
+        grid.get_gridindices_and_splinevals,
+        return_spline_gradients=return_spline_gradients,
+    )
     vmapped_get_gridindices_and_splinevals = jax.vmap(
-        grid.get_gridindices_and_splinevals
+        get_gridindices_and_splinevals,
     )
 
     def anterpolate(positions_1d, charges):
-        indices, splinevals = vmapped_get_gridindices_and_splinevals(
+        indices, spline_outputs = vmapped_get_gridindices_and_splinevals(
             positions_1d
         )
+        splinevals = spline_outputs[0]
 
         gridcharge = jnp.zeros(grid.n_total)
         gridcharge = gridcharge.at[indices].add(
             charges[:, jnp.newaxis] * splinevals
         )
 
-        return gridcharge, indices, splinevals
+        return gridcharge, indices, spline_outputs
 
     return anterpolate
 
@@ -226,3 +245,143 @@ def create_interaction_operator(
         return outarray
 
     return apply_interaction
+
+
+def create_compute_gridpotential_level_one(
+    grids,
+    kernelstencils,
+    J: npt.ArrayLike,
+    return_spline_gradients: bool = False,
+) -> Callable:
+    J_zeroplus = J[len(J) // 2 :]  # TODO: pass J or J_zeroplus?
+    p = len(J) - 1  # TODO: How should p be passed/inferred?
+
+    max_gridlevel = len(grids) - 1
+
+    anterpolate = create_anterpolation_function(
+        grids[1], return_spline_gradients=return_spline_gradients
+    )
+
+    restriction_funcs = {}
+    for lvl in range(2, max_gridlevel + 1):
+        restrict = make_restriction_operator(
+            grid_source=grids[lvl - 1], grid_target=grids[lvl], J=J
+        )
+        restriction_funcs[lvl] = restrict
+
+    prolongation_funcs = {}
+    for lvl in range(1, max_gridlevel):
+        prolongate = make_prolongation_operator(
+            grid_source=grids[lvl + 1],
+            grid_target=grids[lvl],
+            J_zeroplus=J_zeroplus,
+            p=p,
+        )
+        prolongation_funcs[lvl] = prolongate
+
+    interaction_funcs = {}
+    for lvl in range(1, max_gridlevel + 1):
+        interact = create_interaction_operator(
+            grid=grids[lvl], kernelstencil=kernelstencils[lvl]
+        )
+        interaction_funcs[lvl] = interact
+
+    def compute_gridpotential_level_one(
+        positions: jax.Array, charges: jax.Array
+    ) -> Tuple[jax.Array, jax.Array, jax.Array]:
+        # Compute lowest-level grid charge from particle positions and charges
+        gridcharge_level_one, indices, spline_outputs = anterpolate(
+            positions_1d=positions, charges=charges
+        )
+        gridcharges_all_levels = {1: gridcharge_level_one}
+
+        # Go up ladder
+        for lvl in range(2, max_gridlevel + 1):
+            restrict = restriction_funcs[lvl]
+            gridcharge_fine = gridcharges_all_levels[lvl - 1]
+            gridcharge_coarse = restrict(gridcharge_fine)
+            gridcharges_all_levels[lvl] = gridcharge_coarse
+
+        # Apply top-level interaction
+        gridcharge_toplevel = gridcharges_all_levels[max_gridlevel]
+        interaction_toplevel = interaction_funcs[max_gridlevel]
+        gridpotential = interaction_toplevel(gridcharge_toplevel)
+
+        # Go down ladder
+        for lvl in range(max_gridlevel - 1, 0, -1):
+            gridpotential = interaction_funcs[lvl](
+                gridcharges_all_levels[lvl]
+            ) + prolongation_funcs[lvl](gridpotential)
+
+        return gridpotential, indices, spline_outputs
+
+    return compute_gridpotential_level_one
+
+
+def make_compute_U_oneplus(
+    grids, kernelstencils, J: npt.ArrayLike
+) -> Callable:
+    compute_gridpotential_level_one = create_compute_gridpotential_level_one(
+        grids=grids, kernelstencils=kernelstencils, J=J
+    )
+
+    def compute_U_oneplus(
+        positions: jax.Array, charges: jax.Array
+    ) -> jax.Array:
+        (
+            gridpotential_level_one,
+            indices,
+            spline_outputs,
+        ) = compute_gridpotential_level_one(
+            positions=positions, charges=charges
+        )
+        splinevals = spline_outputs[0]
+        return (
+            0.5
+            * (
+                charges[:, jnp.newaxis]
+                * gridpotential_level_one[indices]
+                * splinevals
+            ).sum()
+        )
+
+    return compute_U_oneplus
+
+
+def make_compute_U_and_f_oneplus(
+    grids, kernelstencils, J: npt.ArrayLike
+) -> Callable:
+    compute_gridpotential_level_one = create_compute_gridpotential_level_one(
+        grids=grids,
+        kernelstencils=kernelstencils,
+        J=J,
+        return_spline_gradients=True,
+    )
+
+    def compute_U_and_f_oneplus(
+        positions: jax.Array, charges: jax.Array
+    ) -> Tuple[jax.Array, jax.Array]:
+        (
+            gridpotential_level_one,
+            indices,
+            spline_outputs,
+        ) = compute_gridpotential_level_one(
+            positions=positions, charges=charges
+        )
+        splinevals = spline_outputs[0]
+        splinegrads = spline_outputs[1]
+        U_0 = (
+            0.5
+            * (
+                charges[:, jnp.newaxis]
+                * gridpotential_level_one[indices]
+                * splinevals
+            ).sum()
+        )
+        f_0 = -charges * (gridpotential_level_one[indices] * splinegrads).sum(
+            axis=1
+        )
+
+        return U_0, f_0
+
+    return compute_U_and_f_oneplus
