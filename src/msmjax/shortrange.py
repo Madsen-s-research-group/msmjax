ab5cc383c4faf975dfcab3c9251b15871a241ef7
Florian <florian.buchner@tuwien.ac.at>
2024-02-27 22:46:41 +0100

    WIP: short-range evaluation closure

--- src/msmjax/shortrange.py
+++ src/msmjax/shortrange.py
@@ -13,21 +13,61 @@
 
 from typing import Callable, Tuple
 
+import jax
+import jax.numpy as jnp
+import numpy as onp
+import numpy.typing as npt
 from jax_md import partition, space
 
 
 def make_evaluate_shortrange_with_neighbor_list(
     shortrange_kernel: Callable,
     cutoff: float,  # TODO
-    displacement_or_metric,
-    box,
+    box: npt.ArrayLike,
+    pbcs: npt.ArrayLike,
     **neighbor_kwargs,
 ) -> Tuple[partition.NeighborFn, Callable]:
+    box = jnp.asarray(box)
+    pbcs = jnp.asarray(pbcs)
+
+    if not (jnp.all(pbcs) or jnp.all(~pbcs)):
+        raise ValueError("Mixed boundary conditions currently not supported.")
+    periodic = pbcs[0]
+
+    if periodic:
+        displacement_fn, shift_fn = space.periodic(box)
+    else:
+        displacement_fn, shift_fn = space.free()
+
     neighbor_fn = partition.neighbor_list(
-        displacement_or_metric=displacement_or_metric,
-        box=box,
-        r_cutoff=cutoff,
-        **neighbor_kwargs,
+        displacement_fn, box, r_cutoff=cutoff, **neighbor_kwargs
     )
 
-    return neighbor_fn
+    def evaluate_one_row_of_neighborlist(idx_of_row, row, dR):
+        nb_particles = dR.shape[0]
+        pair_contribs = jnp.where(
+            row < nb_particles,
+            shortrange_kernel(jnp.linalg.norm(dR[idx_of_row, row], axis=1)),
+            0.0,
+        )
+
+        return jnp.sum(pair_contribs)
+
+    def energy_fn(positions, neighborlist):
+        dR = space.map_product(displacement_fn)(positions, positions)
+
+        all_indices = jnp.arange(neighborlist.idx.shape[0])
+
+        energy = 0.5 * jnp.sum(
+            jax.vmap(evaluate_one_row_of_neighborlist, (0, 0, None), 0)(
+                all_indices, neighborlist.idx, dR
+            )
+        )
+
+        return energy
+
+    return energy_fn, neighbor_fn
+
+
+if __name__ == "__main__":
+    pass
