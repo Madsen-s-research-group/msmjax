47d24dc73c81d99e18f817d6fa9a94cabbbfda48
Florian <florian.buchner@tuwien.ac.at>
2024-02-27 23:36:18 +0100

    WIP: compare short-range neighbor list result with direct loop

--- src/msmjax/shortrange.py
+++ src/msmjax/shortrange.py
@@ -10,6 +10,9 @@
         Forces for the Simulation of Biomolecules (PhD thesis), University
         of Illinois at Urbana-Champaign, 2006.
 """
+import os
+
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 
 from typing import Callable, Tuple
 
@@ -19,16 +22,20 @@ import numpy as onp
 import numpy.typing as npt
 from jax_md import partition, space
 
+from msmjax.kernels import SofteningFunctionOneOverR, split_one_over_r_kernel
+
 
 def make_evaluate_shortrange_with_neighbor_list(
     shortrange_kernel: Callable,
-    cutoff: float,  # TODO
+    cutoff: float,
     box: npt.ArrayLike,
     pbcs: npt.ArrayLike,
     **neighbor_kwargs,
 ) -> Tuple[partition.NeighborFn, Callable]:
     box = jnp.asarray(box)
     pbcs = jnp.asarray(pbcs)
+
+    # TODO: if periodic, check if cutoff <= half of box size
 
     if not (jnp.all(pbcs) or jnp.all(~pbcs)):
         raise ValueError("Mixed boundary conditions currently not supported.")
@@ -66,8 +73,56 @@ def make_evaluate_shortrange_with_neighbor_list(
 
         return energy
 
-    return energy_fn, neighbor_fn
+    return neighbor_fn, energy_fn
 
 
 if __name__ == "__main__":
-    pass
+    sidelength = 10.0
+    ndim = 3
+    level_zero_cutoff = 3.75
+    max_gridlevel = 4
+    p = 4
+    n_particles = 15
+    pbcs = [True] * ndim
+    box = jnp.array([sidelength] * 3)
+
+    rng = onp.random.default_rng(58347)
+    pos = rng.uniform(low=0.0, high=sidelength, size=(n_particles, ndim))
+    chg = rng.uniform(low=-1.0, high=1.0, size=n_particles)
+
+    print(pos)
+    print(chg)
+
+    kernels = split_one_over_r_kernel(
+        max_level=max_gridlevel,
+        level_zero_cutoff=level_zero_cutoff,
+        softening_function=SofteningFunctionOneOverR(p),
+    )
+
+    neighbor_fn, energy_fn = make_evaluate_shortrange_with_neighbor_list(
+        shortrange_kernel=kernels[0],
+        cutoff=level_zero_cutoff,
+        box=box,
+        pbcs=pbcs,
+    )
+    nbl_allocate_fun = neighbor_fn.allocate
+    nbl_update_fun = neighbor_fn.update
+    jitted_nbl_update_fun = jax.jit(nbl_update_fun)
+    jitted_eval_direct_energy = jax.jit(energy_fn)
+
+    neighborlist = nbl_allocate_fun(pos)
+
+    e = jitted_eval_direct_energy(pos, neighborlist)
+    print(e)
+
+    def calculate_energy_reference_loop(positions, charges):
+        out = 0.0
+        for i in range(positions.shape[0]):
+            for j in range(i):
+                r_ij = onp.linalg.norm(positions[i] - positions[j])
+                out += charges[i] * charges[j] * kernels[0](r_ij)
+
+        return out
+
+    e_ref = calculate_energy_reference_loop(pos, chg)
+    print(e_ref)
