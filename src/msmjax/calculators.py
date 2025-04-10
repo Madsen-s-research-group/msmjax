47e490550977095815d203c82acdc2cf3508cc00
Florian <florian.buchner@tuwien.ac.at>
2025-04-10 14:58:55 +0200

    WIP: Start implementing set_up_static_cell_msm

--- src/msmjax/calculators.py
+++ src/msmjax/calculators.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import partial
 from typing import Literal, Sequence
 
 import numpy.typing as npt
@@ -10,6 +11,12 @@ from msmjax.core.longrange import (
     make_dyn_cell_longrange_fn,
     make_grid_pass_fn,
 )
+from msmjax.core.shortrange import (
+    make_compute_u_zero,
+    make_eval_pair_pot,
+    make_eval_pair_pot_with_neighbor_list,
+)
+from msmjax.kernels import SofteningFunctionOneOverR, split_one_over_r_kernel
 
 # TODO: This should be defined elsewhere, since the longrange part will likely
 #  also use it
@@ -42,7 +49,7 @@ class StaticCellMSMParams:
     # TODO: Make optional? Interaction with `use_neighbor_list`?
     #  (Where to) check if cutoff fits?
     supercell_diag: Sequence[int]
-    use_neighbor_list: bool  # TODO: Interaction with `supercell_diag`?
+    use_neighborlist: bool  # TODO: Interaction with `supercell_diag`?
     ...
     # -------------------------------------------------------------------------
     # Long-range evaluation
@@ -56,10 +63,54 @@ class StaticCellMSMParams:
     #  model attribute. Users should be allowed to run the same model on
     #  different numbers of particles.
     n_particles: int
+    omega: ArrayLike  # TODO: Non-negative-index part or full? Include at all?
+    J: ArrayLike  # TODO: Non-negative-index part or full? Include at all? Lowercase name?
+    kernel_stencils: Sequence[ArrayLike]  # TODO: Include??? Type?
 
 
 def set_up_static_cell_msm(params: StaticCellMSMParams):
-    ...
+    kernel_fns = split_one_over_r_kernel(
+        max_level=max_level_split,
+        level_zero_cutoff=level_zero_cutoff,
+        softening_function=SofteningFunctionOneOverR(p),
+    )
+
+    if use_neighborlist:
+        pair_map_fn = partial(
+            make_eval_pair_pot_with_neighbor_list,
+            pbc=pbc,
+            cell_mode=cell_mode,
+        )
+    else:
+        pair_map_fn = partial(
+            make_eval_pair_pot,
+            pbc=pbc,
+            cell_mode=cell_mode,
+            supercell_diag=supercell_diag,
+        )
+
+    compute_u_zero = make_compute_u_zero(
+        kernel_fns=kernel_fns, pair_map_fn=pair_map_fn
+    )
+    compute_u_oneplus = make_compute_u_oneplus(...)  # TODO
+
+    # TODO: Offer versions with and without neighbor list without having to
+    #  define the energy function and all its derivatives twice, inside
+    #  inside both branches of an if-else that switches between
+    #  neighbor list and no neighbor list?
+    def compute_energy(positions, charges, neighbor_list):
+        u_zero = compute_u_zero(
+            positions,
+            charges,
+            cell=cell,
+            neighbor_list=neighbor_list,
+            weights=1.0,
+        )
+        u_oneplus = compute_u_oneplus(positions, charges, kernel_stencils)
+        return u_zero + u_oneplus
+
+    # TODO: Functions for forces, energy and forces, charge gradient, stress, ..?
+    return compute_energy
 
 
 def set_up_dyn_cell_msm(params: DynCellMSMParams):
