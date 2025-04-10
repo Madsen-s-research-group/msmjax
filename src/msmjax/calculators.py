2e90cc889b9909ccd1e33206258ed80105b67e97
Florian <florian.buchner@tuwien.ac.at>
2025-04-10 16:55:03 +0200

    WIP: Misc additions to set_up_static_cell_msm function

--- src/msmjax/calculators.py
+++ src/msmjax/calculators.py
@@ -2,10 +2,13 @@ from dataclasses import dataclass
 from functools import partial
 from typing import Literal, Sequence
 
+import jax
+import jax.numpy as jnp
 import numpy.typing as npt
 from jax import Array
 from jax.typing import ArrayLike
 
+from grid_ops_experiment.grid_ops_experiment_multidim import J_zeroplus
 from msmjax.core.longrange import (
     make_compute_u_oneplus,
     make_dyn_cell_longrange_fn,
@@ -22,6 +25,7 @@ from msmjax.kernels import SofteningFunctionOneOverR, split_one_over_r_kernel
 #  also use it
 # TODO: Name that is meaningful in all places where this is used?
 CellMode = Literal["ortho", "general"]
+ConvMeth = Literal["direct", "fft"]
 
 
 @dataclass
@@ -54,7 +58,8 @@ class StaticCellMSMParams:
     # -------------------------------------------------------------------------
     # Long-range evaluation
     # -------------------------------------------------------------------------
-    ...
+    convolution_methods: Sequence[str]
+    ...  # TODO: all grid stuff
     # -------------------------------------------------------------------------
     # Info
     # -------------------------------------------------------------------------
@@ -75,43 +80,71 @@ def set_up_static_cell_msm(params: StaticCellMSMParams):
         softening_function=SofteningFunctionOneOverR(p),
     )
 
+    static_pair_eval_kwargs = {"pbc": pbc, "cell_mode": cell_mode}
     if use_neighborlist:
         pair_map_fn = partial(
-            make_eval_pair_pot_with_neighbor_list,
-            pbc=pbc,
-            cell_mode=cell_mode,
+            make_eval_pair_pot_with_neighbor_list, **static_pair_eval_kwargs
         )
     else:
-        pair_map_fn = partial(
-            make_eval_pair_pot,
-            pbc=pbc,
-            cell_mode=cell_mode,
-            supercell_diag=supercell_diag,
-        )
+        static_pair_eval_kwargs["supercell_diag"] = supercell_diag
+        pair_map_fn = partial(make_eval_pair_pot, **static_pair_eval_kwargs)
 
+    # TODO: close over cell -> in fact, this happens inside compute_energy
     compute_u_zero = make_compute_u_zero(
         kernel_fns=kernel_fns, pair_map_fn=pair_map_fn
     )
+
+    # TODO: coefficients
+    omega, _ = compute_coeffs_with_truncation(p, mu)
+    J_zeroplus = compute_J_zeroplus(p)
+
+    # TODO: grids
+
+    # TODO: stencils
+
     compute_u_oneplus = make_compute_u_oneplus(...)  # TODO
 
     # TODO: Offer versions with and without neighbor list without having to
-    #  define the energy function and all its derivatives twice, inside
-    #  inside both branches of an if-else that switches between
-    #  neighbor list and no neighbor list?
-    def compute_energy(positions, charges, neighbor_list):
-        u_zero = compute_u_zero(
-            positions,
-            charges,
-            cell=cell,
-            neighbor_list=neighbor_list,
-            weights=1.0,
-        )
-        u_oneplus = compute_u_oneplus(positions, charges, kernel_stencils)
-        return u_zero + u_oneplus
+    #  define the energy function and all its derivatives twice, inside both
+    #  both branches of an if-else that switches between neighbor list and
+    #  no neighbor list?
+    if use_neighborlist:
 
-    # TODO: Functions for forces, energy and forces, charge gradient, stress, ..?
-    return compute_energy
+        def calc_energy(positions, charges, neighbor_list):
+
+            # TODO: Could cell and weights be fixed further up? This would
+            #  allow writing a unified calc_energy function for both with
+            #  and without neighbor list, if we accept that the
+            #  no-neighbor-list version has an (unused) 'neighbor_list'
+            #  parameter as well.
+
+            u_zero = compute_u_zero(
+                positions,
+                charges,
+                cell=cell,
+                neighbor_list=neighbor_list,
+                weights=1.0,
+            )
+            u_oneplus = compute_u_oneplus(positions, charges, kernel_stencils)
+            return u_zero + u_oneplus
+
+    def calc_forces(positions, charges, neighbor_list):
+        return -jax.grad(calc_energy, argnums=0)(
+            positions, charges, neighbor_list
+        )
+
+    def calc_energy_and_forces(positions, charges, neighbor_list):
+        value, grad = jax.value_and_grad(calc_energy, argnums=0)(
+            positions, charges, neighbor_list
+        )
+        return value, -grad
+
+    # TODO: charge gradient?
+
+    return calc_energy, calc_forces, calc_energy_and_forces
 
 
 def set_up_dyn_cell_msm(params: DynCellMSMParams):
-    ...
+    # TODO: Functions for forces, energy and forces, charge gradient, ...
+    #  + stress!!!
+    pass  # TODO
