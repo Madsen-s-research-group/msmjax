439a6fdbfdaa56568e853dae3ba7bc8d4f7361fa
Florian <florian.buchner@tuwien.ac.at>
2025-06-23 17:58:16 +0200

    Speed up cost-vs-accuracy benchmark by pushing neighborlist to outer loop and switching onp to jnp

--- examples/benchmarks/cost_vs_accuracy/run_benchmark.py
+++ examples/benchmarks/cost_vs_accuracy/run_benchmark.py
@@ -6,10 +6,12 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 import jax
+import jax.numpy as jnp
 import numpy as onp
 import pandas as pd
 from jaxlib.xla_extension import XlaRuntimeError
 from matplotlib import pyplot as plt
+from matscipy.neighbours import neighbour_list
 from tqdm import tqdm
 
 from msmjax.calculators import create_msm, set_up_msm_params
@@ -24,6 +26,38 @@ LEVEL_ONE_SPACING = 1.0
 QUANTITY = "energy"
 
 LIST_OF_PS = [4, 6, 8]
+
+
+def build_neighborlists(set_of_positions, set_of_cells, cutoff, pbc):
+    neighbor_lists = []
+    sizes = []
+    n_structures = len(set_of_positions)
+    print(f"- Building neighbor lists for {n_structures} structures")
+    for pos, cll in tqdm(
+        zip(set_of_positions, set_of_cells), total=n_structures
+    ):
+        nbl = neighbour_list(
+            "ij", cutoff=cutoff, positions=pos, cell=cll, pbc=pbc
+        )
+        nbl_no_duplicates = jnp.unique(
+            jnp.sort(jnp.column_stack([nbl[0], nbl[1]]), axis=1), axis=0
+        )
+        nbl_no_duplicates = (nbl_no_duplicates[:, 0], nbl_no_duplicates[:, 1])
+        neighbor_lists.append(nbl_no_duplicates)
+        sizes.append(len(nbl_no_duplicates[0]))
+    target_size = max(sizes)
+    n_particles = set_of_positions.shape[1]
+    placeholder_index = n_particles
+    for idx_structure in range(len(neighbor_lists)):
+        i, j = neighbor_lists[idx_structure]
+        padding = target_size - len(i)
+        neighbor_lists[idx_structure] = (
+            jnp.pad(i, (0, padding), constant_values=placeholder_index),
+            jnp.pad(j, (0, padding), constant_values=placeholder_index),
+        )
+    print("- Finished building neighbor lists")
+    return neighbor_lists
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -46,15 +80,32 @@ if __name__ == "__main__":
     structures = onp.load(npz_file)
     n_structures = len(structures["cells"])
 
-    # TODO: This restriction is only meaningful and necessary in non-periodic case
+    resultsfile = baseoutdir / "results.csv"
+
+    # TODO: This restriction is only sensible and necessary in non-periodic case
     cell = jax.device_put(structures["cells"][0])
     half_sidelength = 0.5 * cell[0, 0]
-    range_of_alphas = onp.arange(3.0, half_sidelength / LEVEL_ONE_SPACING, 1.0)
+    range_of_alphas = onp.arange(
+        3.0, min(half_sidelength / LEVEL_ONE_SPACING, 8.01), 1.0
+    )
     range_of_cutoffs = range_of_alphas * LEVEL_ONE_SPACING
 
-    for p in LIST_OF_PS:
-        print(f"- Looping over cutoffs for {p=}")
-        for level_zero_cutoff in tqdm(range_of_cutoffs):
+    print(f"- Range of cutoffs is {range_of_cutoffs}")
+    print()
+
+    for level_zero_cutoff in range_of_cutoffs:
+        print("#" * 80)
+        print(f"- r_cut_0 = {level_zero_cutoff:.2f}")
+        print("#" * 80)
+        neighborlists = build_neighborlists(
+            structures["positions"],
+            structures["cells"],
+            level_zero_cutoff,
+            pbc=PBC,
+        )
+        print()
+        for p in LIST_OF_PS:
+            print(f"- p = {p}:")
             msm_params = set_up_msm_params(
                 cell=cell,
                 level_one_spacings=LEVEL_ONE_SPACING,
@@ -64,6 +115,8 @@ if __name__ == "__main__":
                 cell_mode="ortho",
                 dynamic_cell=False,
                 n_particles=N_PARTICLES,
+                use_neighborlist=True,
+                neighborlist_prefactor=0.5,  # for no-duplicate neighbor list
             )
             msm_evaluation_fns = create_msm(msm_params)
             timing_fn = make_timed_eval(
@@ -71,13 +124,34 @@ if __name__ == "__main__":
             )
             all_times = []
             all_calculation_results = []
-            for idx_structure in range(n_structures):
+            print(f"- Starting timing loop over {n_structures} structures")
+            for idx_structure in tqdm(range(n_structures)):
+                # TODO: double precision?
                 pos = jax.device_put(structures["positions"][idx_structure])
                 chg = jax.device_put(structures["charges"][idx_structure])
-                # TODO: neighbor list
-                min_time, calculation_result = timing_fn(pos, chg)
+                nbl = jax.device_put(neighborlists[idx_structure])
+                # TODO: try-except out-of-memory errors?
+                min_time, calculation_result = timing_fn(
+                    pos, chg, neighborlist=nbl
+                )
                 all_times.append(min_time)
                 all_calculation_results.append(calculation_result)
 
-            print(all_times)
-            # TODO: save output
+            # TODO: What (else) to save? quantity? pbc?
+            results_tmp = pd.DataFrame(
+                data={
+                    "n_particles": N_PARTICLES,
+                    "level_zero_cutoff": level_zero_cutoff,
+                    "p": p,
+                    "time": onp.mean(all_times),
+                    # "error": error, # TODO
+                },
+                index=[0],
+            )
+            print(f"- Writing results to {resultsfile}.")
+            if not resultsfile.is_file():
+                results_tmp.to_csv(resultsfile, index=False, mode="w")
+            else:
+                results_tmp.to_csv(
+                    resultsfile, index=False, mode="a", header=False
+                )
